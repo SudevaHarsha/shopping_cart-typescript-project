29d79f50d154809971c61139773a6766695112be
SudevaHarsha <sudevaharsha1@gmail.com>
2023-07-06 07:47:18 +0530

    completed

--- React-typescript/src/context/CartProvider.tsx
+++ React-typescript/src/context/CartProvider.tsx
@@ -1,3 +1,5 @@
+import { createContext, useMemo, useReducer, ReactElement } from "react";
+
 export type CartItemType = {
     sku: string,
     name: string,
@@ -30,17 +32,32 @@ const reducer = (state: CartStateType, action: ReducerAction): CartStateType =>{
                 throw new Error("Action payload is missing in ADD")
             }
             const {sku,name,price} = action.payload;
+            const filteredCart:CartItemType[]=state.cart.filter(item=>item.sku!= sku);
+            const itemExists:CartItemType| undefined=state.cart.find(item=>item.sku===sku);
+            const qty:number=itemExists?itemExists?.qty +1: 1;
+            return {...state,cart:[...filteredCart,{sku,name,price,qty}]};
         }
         case REDUCER_ACTION_TYPE.REMOVE : {
             if(!action.payload) {
                 throw new Error("Action payload is missing in REMOVE")
             }
-
+            const {sku} = action.payload;
+            const filteredCart:CartItemType[]=state.cart.filter(item=>item.sku!= sku);
+            return{...state,cart:[...filteredCart]};
         }
         case REDUCER_ACTION_TYPE.QUANTITY : {
             if(!action.payload) {
                 throw new Error("Action payload is missing in QUANTITY")
             }
+            const {sku,qty} = action.payload;
+            
+            const itemExists:CartItemType| undefined=state.cart.find(item=>item.sku===sku);
+            if(!itemExists){
+                throw new Error("item need to be in cart to update");
+            }
+            const updateItem:CartItemType ={...itemExists,qty};
+            const filteredCart:CartItemType[]=state.cart.filter(item=>item.sku!= sku);
+            return {...state,cart:[...filteredCart,updateItem]}
         }
         case REDUCER_ACTION_TYPE.SUBMIT : {
             return {...state, cart:[]}
@@ -48,4 +65,51 @@ const reducer = (state: CartStateType, action: ReducerAction): CartStateType =>{
         default:
             throw new Error("unIdentified reducer action type");
     }
+}
+
+const useCartContext = (initCartState: CartStateType) => {
+    const [state, dispatch] = useReducer(reducer, initCartState)
+
+    const REDUCER_ACTIONS = useMemo(() => {
+        return REDUCER_ACTION_TYPE
+    }, [])
+
+    const totalItems = state.cart.reduce((previousValue, cartItem) => {
+        return previousValue + cartItem.qty
+    }, 0)
+
+    const totalPrice = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(
+        state.cart.reduce((previousValue, cartItem) => {
+            return previousValue + (cartItem.qty * cartItem.price)
+        }, 0)
+    )
+
+    const cart = state.cart.sort((a, b) => {
+        const itemA = Number(a.sku.slice(-4))
+        const itemB = Number(b.sku.slice(-4))
+        return itemA - itemB
+    })
+
+    return { dispatch, REDUCER_ACTIONS, totalItems, totalPrice, cart }
+}
+export type useCartContextType = ReturnType<typeof useCartContext>
+
+const initCartContextState : useCartContextType = {
+    dispatch: () => {},
+    REDUCER_ACTIONS: REDUCER_ACTION_TYPE,
+    totalItems : 0,
+    totalPrice: "",
+    cart: [],
+}
+
+export const CartContext = createContext<useCartContextType> (initCartContextState) 
+
+type childrenType = {children?: ReactElement | ReactElement[] };
+
+export const CartProvider = ({children} : childrenType) : ReactElement =>{
+    return (
+        <CartContext.Provider value={useCartContext(initCartState)}>
+            {children}
+        </CartContext.Provider>
+    )
 }
--- React-typescript/src/main.tsx
+++ React-typescript/src/main.tsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { ProductsProvider } from './context/ProductsProvider.tsx'
+import { CartProvider } from './context/CartProvider.tsx'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <CartProvider>
+      <ProductsProvider>
+        <App />
+      </ProductsProvider>
+    </CartProvider>
   </React.StrictMode>,
 )
